23bc7aa71ea08f5a3626e534aad091791f19d8a2
Zakrieva <zakrieva34@mail.ru>
2024-10-22 01:02:20 +0300

    fix: menu

--- app/components/homePage/menu.tsx
+++ app/components/homePage/menu.tsx
@@ -20,7 +20,6 @@ export const MenuWhile = () => {
   const [isScrolled, setIsScrolled] = useState(false);
 
   const handleScroll = () => {
-    
     if (window.innerWidth >= 1280) { 
       setIsScrolled(window.scrollY > 195);
     } else {
@@ -42,11 +41,13 @@ export const MenuWhile = () => {
   return (
     <Container>
       <div className={`xl:flex hidden justify-between items-center 
-        ${isScrolled ? 'fixed z-10 top-0 transition-all duration-300' : ''}`}>
+        ${isScrolled ? 'fixed z-10 top-0 transition-all duration-500 ease-in-out shadow-lg' : 'transition-all duration-500 ease-in-out'}`}>
         
-        <ul className="flex justify-center items-center gap-2 rounded-[15px] h-[55px] bg-[#fafafa]">
+        <ul className="flex justify-center items-center gap-2 rounded-[15px] h-[55px] bg-[#fafafa] transition-all duration-500 ease-in-out">
           {menu.map(({ id, name }) => (
-            <li className='pt-[13px] pr-[24px] pb-[13px] pl-[24px] hover:text-red rounded-[15px] hover:shadow-xl active:shadow-2xl transition-shadow duration-300 ease-in-out' key={id}>
+            <li className='pt-[13px] pr-[24px] pb-[13px] pl-[24px] hover:text-red hover:bg-white rounded-[15px] 
+            hover:shadow-[0px_4px_4px_rgba(0,0,0,0.05)] active:shadow-2xl transition-color transition-shadow duration-300 ease-in-out' 
+            key={id}>
               <Link href={`#`}>
                 {name}
               </Link>
@@ -66,9 +67,10 @@ export const MenuWhile = () => {
             </select>
           </div>
         </ul>
-        <button className="w-[113px] h-[43px] p-[12px] rounded-[50px] bg-[var(--green)] text-white">Корзина</button>
+        <button className="w-[113px] h-[43px] p-[12px] rounded-[50px] bg-[var(--green)] text-white">
+          Корзина
+        </button>
       </div>
-      
     </Container>
   );
 };
@@ -98,9 +100,9 @@ export const MenuMobile = () => {
 
   return (
     <div className={`xl:hidden flex justify-end items-center 
-      ${isScrolled ? 'fixed z-10 top-0 transition-all duration-300' : ''}`}>
+      ${isScrolled ? 'fixed z-10 top-0 transition-all duration-500 ease-in-out shadow-lg' : 'transition-shadow duration-500 ease-in-out'}`}>
       
-      <div className=" w-[98vw] rounded-[20px]   h-[55px] ">
+      <div className="w-[98vw] rounded-[20px] h-[55px]">
         <Swiper
           slidesPerView='auto'
           spaceBetween={8}
@@ -111,7 +113,9 @@ export const MenuMobile = () => {
           className="mySwiper"
         >
           {menu.map(({ id, name }) => (
-            <SwiperSlide className='w-[auto!important] h-[auto!important] pt-[8px] pr-[15px] pb-[8px] pl-[15px] bg-[#F3F2F8] text-[#7B8290] text-[18px] hover:text-[#15523E] rounded-[15px]  hover:bg-[#5BA48C4D] active:text-[#15523E] active:bg-[#5BA48C4D]' key={id}>
+            <SwiperSlide className='w-[auto!important] h-[auto!important] pt-[8px] pr-[15px] pb-[8px] 
+            pl-[15px] bg-[#F3F2F8] text-[#7B8290] text-[18px] hover:text-[#15523E] rounded-[20px]
+             hover:bg-[#5BA48C4D] active:text-[#15523E] active:bg-[#5BA48C4D] transition-сolor duration-300 ease-in-out' key={id}>
               <Link href={`#`}>
                 {name}
               </Link>
@@ -121,7 +125,5 @@ export const MenuMobile = () => {
       </div>
       
     </div>
-    
-    
   );
 };
