15100ca8842078a06d8c416dcab553dea8750f3b
Zakrieva <zakrieva34@mail.ru>
2024-11-22 09:10:07 +0300

    fix: вынесла productModal

--- app/components/homePage/button.tsx
+++ app/components/homePage/button.tsx
@@ -1,24 +1,17 @@
 "use client";
-import { Logo, ButtonAdd, Counter } from "../shared";
 import { useState, useEffect } from "react";
-import Image from "next/image";
-import { ProductService, IProduct } from "@/services";
-
+import {  ProductModal } from "../shared";
+import { useRouter } from "next/navigation"; 
 interface ButtonProps {
   value: string;
   id: number;
 }
 
 export const Button: React.FC<ButtonProps> = ({ value, id }) => {
-  const [product, setProduct] = useState<IProduct | null>(null);
-  useEffect(() => {
-    ProductService.getProduct(id).then((res) => setProduct(res));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  console.log(setProduct);
+  const router = useRouter();
+  const [isOpen, setOpen] = useState(false); // Состояние для управления модальным окном
 
-  const [isOpen, setOpen] = useState(false);
-
+  
   useEffect(() => {
     if (isOpen) {
       document.body.style.overflow = "hidden";
@@ -31,20 +24,20 @@ export const Button: React.FC<ButtonProps> = ({ value, id }) => {
     };
   }, [isOpen]);
 
-  const openModal = () => {
-    setOpen(true);
-    //router.push(`${window.location.pathname}?modal=open`, undefined, { shallow: true });
+  const openModal = (id: number) => {
+    const currentPath = window.location.pathname; // Текущий путь
+    router.push(`${currentPath}?modal=${id}`); // Добавляем параметр в URL
   };
-
+  
   const closeModal = () => {
-    setOpen(false);
-    // router.push(window.location.pathname, undefined, { shallow: true });
+    const currentPath = window.location.pathname; // Текущий путь
+    router.replace(currentPath); // Убираем параметр modal из URL
   };
-
   return (
     <div>
+      {/* Кнопка для открытия модального окна */}
       <button
-        onClick={openModal}
+        onClick={() => openModal(id)}
         className="flex justify-center items-center 
           w-[76px] h-[29px] sm:w-[80px] sm:h-[30px] lg:w-[110px] lg:h-[42px] p-[12px] 
           rounded-[50px] bg-[rgba(91,164,140,0.3)]  
@@ -54,97 +47,11 @@ export const Button: React.FC<ButtonProps> = ({ value, id }) => {
         {value}
       </button>
 
-      {isOpen && product && (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div
-            className="fixed inset-0 bg-black opacity-[0.8] cursor-pointer"
-            onClick={closeModal}
-          ></div>
-
-          <div
-            className="relative sm:flex  sm:mx-[28px] sm:[m-0] sm:gap-[20px] md:gap-[34px] lg:gap-[41px] bg-white 
-          w-full sm:w-auto lg:w-[860px] h-screen sm:h-[317px] lg:h-[383px] 
-          py-[20px] sm:p-[15px] md:p-[20px] lg:p-[25px] 
-          sm:rounded-[20px] lg:rounded-[25px] shadow-lg"
-          >
-            <div className="flex sm:hidden p-[20px] py-0 mb-[20px] justify-between items-center ">
-              <Image
-                onClick={closeModal}
-                src={"/ago.svg"}
-                alt={""}
-                width={1300}
-                height={374}
-                className="object-cover cursor-pointer w-[28px] h-[28px]  "
-              />
-              <Logo className="w-[103px]" />
-
-              <Image
-                onClick={closeModal}
-                src={"/close.svg"}
-                alt={""}
-                width={1300}
-                height={374}
-                className="object-cover cursor-pointer w-[28px] h-[28px]  "
-              />
-            </div>
-            <div
-              onClick={closeModal}
-              className="sm:flex hidden  absolute top-5 right-5 "
-            >
-              <Image
-                onClick={closeModal}
-                src={"/close.svg"}
-                alt={""}
-                width={1300}
-                height={374}
-                className="object-cover cursor-pointer w-[28px] h-[28px]  "
-              />
-            </div>
-            <Image
-              src={product.image}
-              alt={""}
-              width={1300}
-              height={374}
-              className="sm:rounded-[10px] object-cover w-[100%] sm:w-[275px] lg:w-[331px] h-[289px] sm:h-[100%] "
-            />
-            <div className="flex px-[20px] flex-col  justify-between  ">
-              <div>
-                <h1
-                  className="mt-[24px] sm:mt-0 
-              text-[20px] sm:text-[27px] lg:text-[32px] 
-              font-[500] sm:font-[600] lg:font-[700] "
-                >
-                  {product.name}
-                </h1>
-                <p className="text-[var(--stroke-2)] mt-[12px] sm:mt-[10px]  lg:mt-[12px] mb-[14px] ">
-                  {product.weight}
-                </p>
-                <p className="text-[var(--stroke-2)]  ">
-                  {product.description}
-                </p>
-                <p className="sm:hidden block absolute right-0 mt-[24px] mr-[20px]  text-[20px] ">
-                  {product.price} ₽
-                </p>
-              </div>
-
-              <div className="flex flex-col  items-end justify-end sm:absolute sm:bottom-5  sm:right-5">
-                <p className="hidden sm:block  sm:mb-[14px] lg:mb-[20px] sm:text-[20px] lg:text-[24px] ">
-                  {product.price}₽
-                </p>
-                <div
-                  className="p-[20px] w-full sm:p-0 absolute bottom-0  right-0 sm:static 
-                "
-                >
-                  <div className="flex  gap-[12px] sm:gap-[15px] lg:gap-[20px]">
-                    <Counter />
-                    <ButtonAdd />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      {/* Модальное окно */}
+      {isOpen && (
+        <ProductModal id={id} onClose={closeModal} />
       )}
     </div>
   );
 };
+
--- app/components/homePage/menuSection.tsx
+++ app/components/homePage/menuSection.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { useState, useEffect, useRef } from "react";
-import { Title, Container } from "../shared";
+import { useSearchParams, useRouter } from "next/navigation"; // Для работы с URL
+import { Title, Container, ProductModal } from "../shared";
 import { SectionService, ISection, API } from "@/services";
 import { CardProduct } from "./cardProduct";
 import { useSectionStore } from "@/store/section";
@@ -9,13 +10,16 @@ import { useSectionStore } from "@/store/section";
 export const MenuSection = () => {
   const { activeId, setActiveId } = useSectionStore();
   const [sections, setSections] = useState<ISection[]>([]);
+  const sectionRefs = useRef<(HTMLDivElement | null)[]>([]);
+
+  const searchParams = useSearchParams();
+  const modalId = searchParams.get("modal");
+  const router = useRouter();
 
   // Fetch sections from the server
   useEffect(() => {
     SectionService.getSection().then((res) => setSections(res));
   }, []);
-
-  const sectionRefs = useRef<(HTMLDivElement | null)[]>([]);
 
   useEffect(() => {
     const options = {
@@ -40,13 +44,24 @@ export const MenuSection = () => {
     };
   }, [setActiveId]);
 
+  const openModal = (id: number) => {
+    const currentPath = window.location.pathname; // Текущий путь
+    router.push(`${currentPath}?modal=${id}`); // Добавляем параметр в URL
+  };
+  
+  const closeModal = () => {
+    const currentPath = window.location.pathname; // Текущий путь
+    router.replace(currentPath); // Убираем параметр modal из URL
+  };
+
   return (
-    <div className="">
+    <div className="menu-section">
       <Container>
         <div className="mt-[24px] sm:mt-[32px]">
           {sectionRefs &&
             sections.map(({ id, title, products }, index) => (
               <div
+                onClick={() => openModal(id)}
                 key={id}
                 id={`section-${id}`}
                 ref={(el) => {
@@ -61,20 +76,26 @@ export const MenuSection = () => {
                 <Title value={title} />
                 <div className="mt-[24px] sm:mt-[32px] lg:mt-[36px] grid grid-cols-1 sm:grid-cols-3 2xl:grid-cols-4 gap-[20px_15px] sm:gap-[32px_20px] lg:gap-[60px_20px]">
                   {products.map(({ id, name, image, description, price }) => (
-                    <CardProduct
-                      key={id}
-                      id={id}
-                      name={name}
-                      image={API + image}
-                      description={description}
-                      price={price}
-                    />
+                    <div key={id}>
+                      <CardProduct
+                        id={id}
+                        name={name}
+                        image={API + image}
+                        description={description}
+                        price={price}
+                      />
+                      
+                    </div>
                   ))}
                 </div>
               </div>
             ))}
         </div>
       </Container>
+
+      {/* Модальное окно для продукта */}
+      {modalId && <ProductModal id={parseInt(modalId)} onClose={closeModal} />}
     </div>
   );
 };
+
