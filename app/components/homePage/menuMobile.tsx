af6e8ab292aedf997aba01a5e1221ac2e74c8a1b
Zakrieva <zakrieva34@mail.ru>
2024-11-25 02:23:08 +0300

    fix: исправила меню

--- app/components/homePage/menuMobile.tsx
+++ app/components/homePage/menuMobile.tsx
@@ -1,9 +1,9 @@
 "use client";
 
+import "swiper/css";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
 import { MenuService, IMenu } from "@/services";
 import { useSectionStore } from "@/store/section";
 import { ButtonCart } from "../shared";
@@ -13,6 +13,8 @@ export const MenuMobile = () => {
   const [menu, setMenu] = useState<IMenu[] | []>([]);
   const [isScrolled, setIsScrolled] = useState(false);
   const { activeId, setActiveId } = useSectionStore();
+  
+  
   useEffect(() => {
     MenuService.getMenu().then((res) => setMenu(res));
   }, []);
@@ -51,15 +53,18 @@ export const MenuMobile = () => {
           >
             {menu.map(({ id, title }, index) => (
               <SwiperSlide
+              key={id}
                 onClick={() => setActiveId(id)}
                 className={`w-[auto!important] h-[auto!important] pt-[8px] pr-[15px] pb-[8px] 
                   pl-[15px] bg-[#F3F2F8] text-[#7B8290] text-[18px] hover:text-[#15523E] rounded-[20px]
-                  hover:bg-[#5BA48C4D] active:text-[#15523E] active:bg-[#5BA48C4D] transition-color duration-300 ease-in-out ${
-                    index === 0 ? "ml-[20px]" : ""
-                  } ${
+                  hover:bg-[#5BA48C4D] active:text-[#15523E] active:bg-[#5BA48C4D] transition-color duration-300 ease-in-out 
+                  ${
+                    index === 0 ? "ml-[20px] " : " "
+                  } 
+                  ${
                     activeId === id ? " text-[#15523E] bg-[#5BA48C4D]" : ""
                   }`}
-                key={id}
+                
               >
                 <Link href={`#section-${id}`}>{title}</Link>
               </SwiperSlide>
--- app/components/homePage/menuWhile.tsx
+++ app/components/homePage/menuWhile.tsx
@@ -11,12 +11,15 @@ import { ButtonCart } from "../shared";
 export const MenuWhile = () => {
   const [menu, setMenu] = useState<IMenu[] | []>([]);
   const { activeId, setActiveId } = useSectionStore(); 
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   useEffect(() => {
     MenuService.getMenu().then((res) => setMenu(res));
   }, []);
 
-  const [isScrolled, setIsScrolled] = useState(false);
+  
 
   const handleScroll = () => {
     if (window.innerWidth >= 1280) {
@@ -32,7 +35,12 @@ export const MenuWhile = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+  const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
 
+  const handleOptionClick = (value: string) => {
+    setSelectedOption(value);
+    setIsDropdownOpen(false);
+  };
   return (
     <div className="">
       <div
@@ -59,15 +67,40 @@ export const MenuWhile = () => {
             {menu.length <= 7 ? (
               ""
             ) : (
-              <div className="relative p-4">
-                <button
-                  onClick={() => {}}
-                  className="flex gap-[10px] p-2 bg-[#fafafa] items-center text-gray-700 rounded-md focus:outline-none"
-                >
-                  {"Ещё"}
-                  <Image src={"/select.png"} alt="" width={22} height={22} />
-                </button>
-              </div>
+              <div
+              className="relative p-4"
+              onMouseEnter={() => setIsDropdownOpen(true)}
+              onMouseLeave={() => setIsDropdownOpen(false)}
+            >
+              <button
+                onClick={toggleDropdown}
+                className="flex gap-[10px] p-2 bg-[#fafafa] text-gray-700 rounded-md focus:outline-none"
+              >
+                {selectedOption || "Ещё"}
+                <Image
+                  src={"/select.png"}
+                  alt=""
+                  width={22}
+                  height={22}
+                  className=" "
+                />
+              </button>
+
+              {isDropdownOpen && (
+                <div className="absolute z-10 w-[190px] mt-[15px] bg-[#fafafa] rounded-[8px] shadow-lg ">
+                  {menu.slice(7, 10).map(({ id, title }) => (
+                    <div
+                      key={id}
+                      onClick={() => handleOptionClick(title)}
+                      className="px-4 py-2 cursor-pointer rounded-[8px] hover:text-red hover:bg-white 
+            hover:shadow-[0px_4px_4px_rgba(0,0,0,0.05)]"
+                    >
+                      <Link href={`#section-${id}`}>{title}</Link>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
             )}
           </ul>
           <ButtonCart/>
